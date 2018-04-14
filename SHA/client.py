a211d28ff6d3eabf72e2d7ab25249d2e4030cf54
Vaibhav Bhapkar <35737937+VaibhavBhapkar@users.noreply.github.com>
2018-04-14 03:48:35 -0700

    Update client.py

--- SHA/client.py
+++ SHA/client.py
@@ -1,5 +1,6 @@
 import hashlib
 import socket
+import time
 if __name__=="__main__":
 	s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	host="127.0.0.1"
@@ -11,6 +12,7 @@ if __name__=="__main__":
 		s.send(str(msg))
 		msgdigest=hashlib.sha1(msg).hexdigest()
 		print "Sending Digest :"+str(msgdigest)
+		time.sleep(2)
 		s.send(str(msgdigest))
 	finally:
 		print "Closing Socket..."
